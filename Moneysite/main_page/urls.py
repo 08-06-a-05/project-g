51c470e843425c5b649efd1ad40d98327d132eda
08-06-a-05 <nab.txt@mail.ru>
2021-12-26 00:43:18 +0300

    abra

--- Moneysite/main_page/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns=[
-    path('registration',views.registration_page,name='registration'),
-    path('authorization',views.authorization_page,name='authorization'),
-    path('',views.main_page,name='main'),
-]
