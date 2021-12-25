e2dc32d18afea2264ac9f46f9788fbbef6a95d6b
08-06-a-05 <nab.txt@mail.ru>
2021-12-25 22:09:14 +0300

    Working with css

--- Moneysite/Moneysite/settings.py
+++ Moneysite/Moneysite/settings.py
@@ -119,9 +119,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS=[
-    '/main_page/static'
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
