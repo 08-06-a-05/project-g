573643662352eacf0998fe886422e6db04ccdd6c
08-06-a-05 <nab.txt@mail.ru>
2021-12-28 22:51:54 +0300

    updated db

--- Moneysite/main_page/models.py
+++ Moneysite/main_page/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-
+import datetime
 
 class Users(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
-    email = models.EmailField()
-    login = models.CharField(max_length=30)
-    password = models.CharField(max_length=50)
-    displayed_currency = models.CharField(max_length=30)
-    balance = models.BigIntegerField()
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=300)
+    displayed_currency = models.CharField(max_length=30,default='ruble')
+    balance = models.BigIntegerField(default=0)
 
 
 class Categories(models.Model):
@@ -19,8 +18,8 @@ class Categories(models.Model):
 class Operations(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     operation_type = models.CharField(max_length=30)
-    datetime = models.DateTimeField(auto_now_add=True)
+    datetime = models.DateTimeField(default=datetime.date.today())
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
     amount = models.PositiveBigIntegerField()
-    currency = models.CharField(max_length=30)
+    currency = models.CharField(max_length=30,default='ruble')
     description = models.TextField(blank=True)
