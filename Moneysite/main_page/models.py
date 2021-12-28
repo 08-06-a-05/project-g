72fb6301776e1b2138b62d81e1618fce2eaea9ac
Lev Yuvensky <levayu22@gmail.com>
2021-12-28 12:44:25 +0300

    Add database and make migration

--- Moneysite/main_page/models.py
+++ Moneysite/main_page/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.EmailField()
+    login = models.CharField(max_length=30)
+    password = models.CharField(max_length=50)
+    displayed_currency = models.CharField(max_length=30)
+    balance = models.BigIntegerField()
+
+
+class Categories(models.Model):
+    category_name = models.CharField(max_length=30)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+
+class Operations(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    operation_type = models.CharField(max_length=30)
+    datetime = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    amount = models.PositiveBigIntegerField()
+    currency = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
