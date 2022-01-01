ec2291ae71a76e3586961d5bf6cc50f9c7a48cad
tsogp <tsogp@d69+Kg6dQ1VX4zdyiaHaTP4GgUM/+v17f4qLHQhmARQ>
2022-01-01 20:27:30 +0300

    password_reset urls removed

--- Moneysite/account_manager/urls.py
+++ Moneysite/account_manager/urls.py
@@ -8,34 +8,6 @@ urlpatterns=[
     path('login/',views.authorization_page, name='login'),
     path('logout/', views.logout_request, name='logout'),
     path('', views.main_page, name='main'),
-
-    path('password_reset/', 
-        auth_views.PasswordResetView.as_view(
-            template_name="password_reset/password_reset.html"
-        ), 
-        name="password_reset",
-    ),
-
-    path('password_reset_sent/', 
-        auth_views.PasswordResetDoneView.as_view(
-            template_name="password_reset/password_reset_sent.html"
-        ), 
-        name="password_reset_done",
-    ),
-
-    path('password_reset/<uidb64>/<token>/', 
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name='password_reset/password_reset_page.html'
-        ), 
-        name="password_reset_confirm"
-        ),
-    path(
-        'password_reset_complete/', 
-        auth_views.PasswordResetCompleteView.as_view(
-            template_name="password_reset/password_reset_done.html"
-        ), 
-        name="password_reset_complete",
-    ),
     
     path('activate_user/<uidb64>/<token>', views.activate_user, name='activate'),
 ]
