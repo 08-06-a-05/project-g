b16e570366b17f73fca4d4941aeebf504020ecf2
08-06-a-05 <nab.txt@mail.ru>
2022-02-12 00:23:31 +0300

    Updated forms on LK

--- Moneysite/operations/static/dropdown.js
+++ Moneysite/operations/static/dropdown.js
@@ -1,7 +1,17 @@
-var content = document.getElementById("dropdown");
-var body = document.body
+var add_category = document.getElementById("new_category");
+var category = document.getElementById("category");
+var swap_button = document.getElementById('swap-category')
+swap_button.addEventListener("click", function (event) {
+    if (swap_button.textContent == "Добавить категорию"){
+        category.style.display = "none";
+        add_category.style.display = "flex";
+        swap_button.textContent="Выбрать из уже существующих";
+        swap_button.style.width = "60%";'
 
-content.addEventListener("click", function (event) {
-    console.log('ura');
-    //body.style.filter = "blur(15px)";
+    } else {
+        category.style.display = "flex";
+        add_category.style.display = "none";
+        swap_button.textContent="Добавить категорию";
+        swap_button.style.width = "40%";
+    }
 });
